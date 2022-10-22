ccd9de11c2793f9a8faa22cc1e5c2c166a85c10d
1MatejaS1 <115102398+1MatejaS1@users.noreply.github.com>
2022-10-22 21:35:49 +0100

    Update Tangent_Line.py

--- Tangent_Line.py
+++ Tangent_Line.py
@@ -8,7 +8,7 @@ plt.grid()
 plt.xlabel("x")
 plt.ylabel("$tan(x)$")
 plt.ylim(-10,10)
-plt.xlim(-2 * np.pi, 2 * np.pi)
+plt.xlim(-2 * np.pi, 2 * np.pi) #can be extended
 radian_multiples = [-2, -3/2, -1, -1/2, 0, 1/2, 1, 3/2, 2]
 radians = [n * np.pi for n in radian_multiples]
 radian_labels = ['$-2\pi$', '$-3\pi/2$', '$\pi$', '$-\pi/2$', '0', '$\pi/2$', '$\pi$', '$3\pi/2$', '$2\pi$']
